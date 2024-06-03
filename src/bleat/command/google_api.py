f32a19e45d68016c7f1393ee294876f3706e4673
Mustafa Hasanbulli <mustafa@hasanbul.li>
2024-06-03 08:46:40 +0000

    Formatting

--- src/bleat/command/google_api.py
+++ src/bleat/command/google_api.py
@@ -1,7 +1,1 @@
-import os
 
-import typer
-from typing_extensions import Annotated
-from bleat.util.common import check_api_key
-
-from rich import print
--- src/bleat/util/common.py
+++ src/bleat/util/common.py
@@ -1,5 +1,5 @@
-from rich import print
 import os
+
 
 def check_api_key() -> bool:
     if not os.getenv("GOOGLE_API_KEY"):
